87853e43e99dc3b98f93177587b05513c9fb26a1
Dmitrii <kopenkin.da@gmail.com>
2023-06-11 04:07:20 +0200

    feat: todo modal, everything except tags is done

--- src/components/todo-modal.tsx
+++ src/components/todo-modal.tsx
@@ -1,5 +1,11 @@
 import {
+  Box,
   Button,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Input,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -7,10 +13,15 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Stack,
+  Switch,
+  Textarea,
 } from "@chakra-ui/react";
 import { useTodos } from "../hooks/use-todos";
-import { type Todo } from "../schemas/todo-schema";
-import { IconPlus } from "@tabler/icons-react";
+import { type Todo, newTodoSchema } from "../schemas/todo-schema";
+import { IconDeviceFloppy, IconPlus } from "@tabler/icons-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export type TodoModalProps = (
   | { mode: "add" }
@@ -20,29 +31,102 @@ export type TodoModalProps = (
   onClose: () => void;
 };
 
-export const TodoModal = ({ mode, onClose, open }: TodoModalProps) => {
+export const TodoModal = ({ onClose, open, ...mode }: TodoModalProps) => {
   const { actions } = useTodos();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Todo>({
+    resolver: zodResolver(newTodoSchema),
+    defaultValues:
+      mode.mode === "edit"
+        ? {
+            title: mode.values.title,
+            description: mode.values.description,
+            tags: mode.values.tags,
+            isCompleted: mode.values.isCompleted,
+          }
+        : { title: "", description: "", tags: [], isCompleted: false },
+  });
+  const onSubmit = (data: Todo) => {
+    if (mode.mode === "edit") {
+      actions.edit(mode.values.id, {
+        title: data.title,
+        description: data.description,
+        tags: data.tags,
+        isCompleted: data.isCompleted,
+      });
+    } else {
+      actions.add({
+        title: data.title,
+        description: data.description,
+        tags: data.tags,
+      });
+    }
+    onClose();
+  };
   return (
     <Modal isOpen={open} onClose={onClose} size="full">
       <ModalOverlay />
-      <ModalContent>
+      <ModalContent as="form" onSubmit={handleSubmit(onSubmit)}>
         <ModalHeader>Add todo</ModalHeader>
         <ModalCloseButton />
-        <ModalBody></ModalBody>
+        <ModalBody>
+          <Stack spacing="4">
+            <FormControl isRequired>
+              <FormLabel>Title</FormLabel>
+              <Input {...register("title")} placeholder='Ex: "Buy milk"' />
+              {errors.title === undefined ? (
+                <FormHelperText>
+                  Please provide a short, meaningful title
+                </FormHelperText>
+              ) : (
+                <FormErrorMessage>{errors.title.message}</FormErrorMessage>
+              )}
+            </FormControl>
+            <FormControl>
+              <FormLabel>Description</FormLabel>
+              <Textarea
+                {...register("description")}
+                placeholder='Ex: "Buy milk"'
+              />
+              {errors.description === undefined ? (
+                <FormHelperText>
+                  Describe the task in more detail
+                </FormHelperText>
+              ) : (
+                <FormErrorMessage>
+                  {errors.description.message}
+                </FormErrorMessage>
+              )}
+            </FormControl>
+            {mode.mode === "edit" && (
+              <FormControl>
+                <Box display="flex" alignItems="center" gap=".5rem">
+                  <FormLabel margin="0">Completed</FormLabel>
+                  <Switch {...register("isCompleted")} />
+                </Box>
+                <FormHelperText>
+                  Mark as completed if you have already done this task
+                </FormHelperText>
+              </FormControl>
+            )}
+          </Stack>
+        </ModalBody>
         <ModalFooter>
           <Button
             colorScheme="green"
-            onClick={() => {
-              actions.add({
-                title: "New todo",
-                description: "",
-                tags: [],
-              });
-              onClose();
-            }}
-            rightIcon={<IconPlus size={16} />}
+            type="submit"
+            rightIcon={
+              mode.mode === "add" ? (
+                <IconPlus size={16} />
+              ) : (
+                <IconDeviceFloppy size={16} />
+              )
+            }
           >
-            Add
+            {mode.mode === "add" ? "Add" : "Save"}
           </Button>
         </ModalFooter>
       </ModalContent>
--- src/schemas/todo-schema.ts
+++ src/schemas/todo-schema.ts
@@ -4,8 +4,11 @@ export const todoSchema = z.object({
   id: z.string(),
   title: z.string(),
   description: z.string(),
-  isCompleted: z.boolean(),
+  isCompleted: z.boolean().default(false),
   tags: z.array(z.string()),
 });
 
+export const newTodoSchema = todoSchema.omit({ id: true });
+
 export type Todo = z.infer<typeof todoSchema>;
+export type NewTodo = z.infer<typeof newTodoSchema>;
