930f202656cbe64253f0f1b53e10df74848a854f
Dmitrii <kopenkin.da@gmail.com>
2023-06-11 17:05:51 +0200

    feat(todo-list): Show CTA when no todos

--- src/components/add-todo.tsx
+++ src/components/add-todo.tsx
@@ -3,24 +3,26 @@ import { IconPlus } from "@tabler/icons-react";
 import { useState, type ComponentProps } from "react";
 import { TodoModal } from "./todo-modal";
 
-export function AddTodo(
-  props: Omit<
-    ComponentProps<typeof Button>,
-    "onClick" | "size" | "aspectRatio" | "padding" | "colorScheme" | "isLoading"
-  >
-) {
+export function AddTodo({
+  withText = false,
+  ...props
+}: Omit<
+  ComponentProps<typeof Button>,
+  "onClick" | "size" | "aspectRatio" | "padding" | "colorScheme" | "isLoading"
+> & { withText?: boolean }) {
   const [modalOpen, setModalOpen] = useState(false);
   return (
     <>
       <Button
         onClick={() => setModalOpen(true)}
         size="sm"
-        aspectRatio={1}
-        padding={1}
+        aspectRatio={withText ? undefined : 1}
+        padding={withText ? undefined : 1}
         colorScheme="green"
         isLoading={modalOpen}
         {...props}
       >
+        {withText && "Add Todo"}
         <IconPlus size={16} />
       </Button>
       <TodoModal
--- src/components/todo-list.tsx
+++ src/components/todo-list.tsx
@@ -1,14 +1,34 @@
-import { Stack } from "@chakra-ui/react";
+import { Box, Stack, Text } from "@chakra-ui/react";
 import { useTodos } from "../hooks/use-todos";
 import { TodoItem } from "./todo-item";
+import { AddTodo } from "./add-todo";
 
 export function TodoList() {
-  const { todos } = useTodos();
+  const { todos, forDate } = useTodos();
+  const today = new Date();
+  const when: "before" | "after" | "today" =
+    today > forDate ? "before" : today < forDate ? "after" : "today";
   return (
     <Stack>
       {todos.map((todo) => (
         <TodoItem key={todo.id} todo={todo} />
       ))}
+      {todos.length === 0 && (
+        <Box
+          pt="24"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          gap=".5rem"
+        >
+          <Text>
+            You {when === "before" ? "had" : "have"} nothing to do on{" "}
+            {when === "today" ? "today" : forDate.toDateString()}
+          </Text>
+          <AddTodo withText />
+        </Box>
+      )}
     </Stack>
   );
 }
